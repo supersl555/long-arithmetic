e2a395c388556e691ddd6d1e75e824cd23b65000
Vladimir <superel555@gmail.com>
2024-02-19 19:42:33 +0600

    Optimized addition and subtraction, and also fixed comparison work. Updated cmake files

--- long-arithmetic/implementation.cpp
+++ long-arithmetic/implementation.cpp
@@ -86,7 +86,18 @@ bool LongNumber::operator>(const LongNumber& other) const {
     }
 
     size1 = num1Parts.second.size(), size2 = num2Parts.second.size();
-    int size = std::min(size1, size2);
+    // Выравниваем дробные части
+    if (size1 > size2){
+        for (int i = 0; i < size1 - size2; ++i){
+            num2Parts.second += "0";
+        }
+    } else if (size1 < size2){
+        for (int i = 0; i < size2 - size1; ++i){
+            num1Parts.second += "0";
+        }
+    }
+
+    int size = num1Parts.second.size();
 
     for (size_t i = 0; i < size; ++i) {
         if (num1Parts.second[i] > num2Parts.second[i]) {
@@ -96,17 +107,70 @@ bool LongNumber::operator>(const LongNumber& other) const {
         }
     }
 
-    return size1 > size2;
+    return false;
 
 }
 
 bool LongNumber::operator==(const LongNumber& other) const {
-    bool larger = (*this) > other ? true : false;
-    if (larger) {
+    std::string& digitsRef = const_cast<std::string&>(digits);
+    if (digitsRef == "-0"){
+        digitsRef = "0";
+    }
+
+    std::string& otherdigitsRef = const_cast<std::string&>(other.digits);
+    if (otherdigitsRef == "-0"){
+        otherdigitsRef = "0";
+    }
+
+    bool isNegative1 = (digits[0] == '-');
+    bool isNegative2 = (other.digits[0] == '-');
+
+
+    if (isNegative1 != isNegative2){
+        return false;
+    }
+
+    std::pair<std::string, std::string> num1Parts = splitNumber(digits);
+    std::pair<std::string, std::string> num2Parts = splitNumber(other.digits);
+
+    int size1 = num1Parts.first.size(), size2 = num2Parts.first.size();
+    if (size1 > size2) {
+        return false;
+    } else if (size1 < size2) {
         return false;
     } else {
-        return other > (*this) ? false : true;
+        for (size_t i = 0; i < size1; ++i) {
+            if (num1Parts.first[i] > num2Parts.first[i]) {
+                return false;
+            } else if (num1Parts.first[i] < num2Parts.first[i]) {
+                return false;
+            }
+        }
     }
+
+    size1 = num1Parts.second.size(), size2 = num2Parts.second.size();
+    // Выравниваем дробные части
+    if (size1 > size2){
+        for (int i = 0; i < size1 - size2; ++i){
+            num2Parts.second += "0";
+        }
+    } else if (size1 < size2){
+        for (int i = 0; i < size2 - size1; ++i){
+            num1Parts.second += "0";
+        }
+    }
+
+    int size = num1Parts.second.size();
+
+    for (size_t i = 0; i < size; ++i) {
+        if (num1Parts.second[i] > num2Parts.second[i]) {
+            return false;
+        } else if (num1Parts.second[i] < num2Parts.second[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool LongNumber::operator!=(const LongNumber& other) const {
@@ -117,8 +181,8 @@ bool LongNumber::operator<(const LongNumber& other) const {
     return other > (*this);
 }
 
-std::vector<int> convertToDigitsVector(const std::string& numberString) {
-    std::vector<int> digits;
+std::vector<char> convertToDigitsVector(const std::string& numberString) {
+    std::vector<char> digits;
     for (char digitChar : numberString) {
         if (isdigit(digitChar)) {
             digits.push_back(digitChar - '0');
@@ -127,7 +191,7 @@ std::vector<int> convertToDigitsVector(const std::string& numberString) {
     return digits;
 }
 
-void alignVectors_1(std::vector<int>& vec1, std::vector<int>& vec2) {
+void alignVectors_1(std::vector<char>& vec1, std::vector<char>& vec2) {
     int size1 = vec1.size();
     int size2 = vec2.size();
     
@@ -144,7 +208,7 @@ void alignVectors_1(std::vector<int>& vec1, std::vector<int>& vec2) {
     }
 }
 
-void alignVectors_2(std::vector<int>& vec1, std::vector<int>& vec2) {
+void alignVectors_2(std::vector<char>& vec1, std::vector<char>& vec2) {
     int size1 = vec1.size();
     int size2 = vec2.size();
     
@@ -161,23 +225,6 @@ void alignVectors_2(std::vector<int>& vec1, std::vector<int>& vec2) {
     }
 }
 
-// Операция сложения
-std::vector<int> addDigits(const std::vector<int>& digits1, const std::vector<int>& digits2) {
-    std::vector<int> sumDigits;
-    int carry = 0;
-    
-    for (int i = digits1.size() - 1; i >= 0; --i) {
-        int sum = digits1[i] + digits2[i] + carry;
-        sumDigits.insert(sumDigits.begin(), sum % 10);
-        carry = sum / 10;
-    }
-    
-    if (carry > 0) {
-        sumDigits.insert(sumDigits.begin(), carry);
-    }
-    
-    return sumDigits;
-}
 
 LongNumber LongNumber::operator+(const LongNumber& other) const {
     // Проверяем на отрицательные числа
@@ -200,29 +247,16 @@ LongNumber LongNumber::operator+(const LongNumber& other) const {
 
     // Вычисляем точность для результата как максимальную из точностей операндов
     int resultPrecision = std::max(precision, other.precision);
-    std::cout << resultPrecision << std::endl;
     // Разбиваем числа на целую и дробную части
     std::pair<std::string, std::string> num1Parts = splitNumber(digits);
     std::pair<std::string, std::string> num2Parts = splitNumber(other.digits);
-
-    // Сложение целой части 
-    // Переводим строки в векторы цифр
-    std::vector<int> digits1 = convertToDigitsVector(num1Parts.first);
-    std::vector<int> digits2 = convertToDigitsVector(num2Parts.first);
-    
-    // Выравниваем векторы по длине
-    alignVectors_1(digits1, digits2);
-    
-    // Выполняем сложение цифр
-    std::vector<int> sumDigits = addDigits(digits1, digits2);
-
 
 
     // Складываем дробные части с учетом точности
     // Преобразуем строки в векторы цифр
     std::string result_2;
-    digits1 = convertToDigitsVector(num1Parts.second);
-    digits2 = convertToDigitsVector(num2Parts.second);
+    std::vector<char> digits1 = convertToDigitsVector(num1Parts.second);
+    std::vector<char> digits2 = convertToDigitsVector(num2Parts.second);
 
     // Добавляем нули в конец векторов, чтобы выровнять их по длине
     alignVectors_2(digits1, digits2);
@@ -235,12 +269,27 @@ LongNumber LongNumber::operator+(const LongNumber& other) const {
         carry = sum / 10;
     }
 
-    // Если остался перенос, добавляем его к целой части
-    if (carry > 0) {
-        std::vector<int> onesVector(1, carry);
-        alignVectors_1(sumDigits, onesVector);
-        sumDigits = addDigits(sumDigits, onesVector);
+    // Сложение целой части 
+    // Переводим строки в векторы цифр
+    digits1 = convertToDigitsVector(num1Parts.first);
+    digits2 = convertToDigitsVector(num2Parts.first);
+    
+    // Выравниваем векторы по длине
+    alignVectors_1(digits1, digits2);
+
+    // Складываем целые части
+    std::vector<char> sumDigits;
+    
+    for (int i = digits1.size() - 1; i >= 0; --i) {
+        int sum = digits1[i] + digits2[i] + carry;
+        sumDigits.insert(sumDigits.begin(), sum % 10);
+        carry = sum / 10;
     }
+    
+    if (carry > 0) {
+        sumDigits.insert(sumDigits.begin(), carry);
+    }
+
 
     // Меняем строку до заданной точности
     int len = result_2.length();
@@ -249,7 +298,6 @@ LongNumber LongNumber::operator+(const LongNumber& other) const {
     } else {
         result_2 = result_2.substr(0, resultPrecision);
     }
-
 
 
     // Преобразуем результаты в строку
@@ -276,40 +324,6 @@ LongNumber LongNumber::operator+(const LongNumber& other) const {
     // Создаем новый объект LongNumber с полученной суммой и точностью
     return LongNumber(sum, resultPrecision);
 }
-// Операция вычитания
-std::vector<int> subtractDigits(const std::vector<int>& digits1, const std::vector<int>& digits2, char flag) {
-    std::vector<int> diffDigits;
-    int borrow = 0;
-    
-    for (int i = digits1.size() - 1; i >= 0; --i) {
-        int diff = digits1[i] - digits2[i] - borrow;
-        if (diff < 0) {
-            diff += 10;
-            borrow = 1;
-        } else {
-            borrow = 0;
-        }
-        diffDigits.insert(diffDigits.begin(), diff);
-    }
-
-    // Удаляем ведущие нули
-    if (flag){
-        while (!diffDigits.empty() && diffDigits.front() == 0) {
-            diffDigits.erase(diffDigits.begin());
-        }
-    }
-
-    return diffDigits;
-}
-
-bool allZeros(const std::vector<int>& vec) {
-    for (int num : vec) {
-        if (num != 0) {
-            return false;
-        }
-    }
-    return true;
-}
 
 LongNumber LongNumber::operator-(const LongNumber& other) const {
     // Вычисляем точность для результата как максимальную из точностей операндов
@@ -334,10 +348,6 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
         return result;
     }
 
-    // Переводим строки в векторы цифр
-    std::vector<int> digits1 = convertToDigitsVector(num1Parts.first);
-    std::vector<int> digits2 = convertToDigitsVector(num2Parts.first);
-
     // Если числа одного знака, определяем большее по модулю
     bool minus = false;
 
@@ -349,31 +359,27 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
         LongNumber num2(str2, other.precision);
         num1IsBigger = num1 > num2;
         if (num1IsBigger){
-            minus = true;
+            minus = num2 > num1 ? false : true; 
         }
     } else {
         num1IsBigger = (*this) > other;
         if (!num1IsBigger){
-            minus = true;
+            minus = other > (*this) ? true : false; 
         }
     }
 
+    // Переводим строки в векторы цифр
+    std::vector<char> digits1 = convertToDigitsVector(num1Parts.second);
+    std::vector<char> digits2 = convertToDigitsVector(num2Parts.second);
 
-    alignVectors_1(digits1, digits2);
+
+    alignVectors_2(digits1, digits2);
     
     // Вычитание в зависимости от размера целой части
-    std::vector<int> diffDigits;
-    std::vector<int> diffDigitsFraction;
+    std::vector<char> diffDigits;
+    std::vector<char> diffDigitsFraction;
 
     if (num1IsBigger) {
-        diffDigits = subtractDigits(digits1, digits2, 1);
-
-        digits1 = convertToDigitsVector(num1Parts.second);
-        digits2 = convertToDigitsVector(num2Parts.second);
-
-        // Добавляем нули в конец векторов, чтобы выровнять их по длине
-        alignVectors_2(digits1, digits2);
-    
         int borrow = 0;
     
         for (int i = digits1.size() - 1; i >= 0; --i) {
@@ -387,21 +393,28 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
             diffDigitsFraction.insert(diffDigitsFraction.begin(), diff);
         }
 
-        if (borrow > 0){
-            std::vector OnesVector(1, 1);
-            alignVectors_1(diffDigits, OnesVector);
-            diffDigits = subtractDigits(diffDigits, OnesVector, 1);
-        }
-
-    } else {
-        diffDigits = subtractDigits(digits2, digits1, 1);
-
-        digits1 = convertToDigitsVector(num1Parts.second);
-        digits2 = convertToDigitsVector(num2Parts.second);
+        digits1 = convertToDigitsVector(num1Parts.first);
+        digits2 = convertToDigitsVector(num2Parts.first);
 
         // Добавляем нули в конец векторов, чтобы выровнять их по длине
-        alignVectors_2(digits1, digits2);
+        alignVectors_1(digits1, digits2);
+    
+        for (int i = digits1.size() - 1; i >= 0; --i) {
+            int diff = digits1[i] - digits2[i] - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else {
+                borrow = 0;
+            }
+            diffDigits.insert(diffDigits.begin(), diff);
+        }
 
+        // Удаляем ведущие нули
+        while (!diffDigits.empty() && diffDigits.front() == 0) {
+            diffDigits.erase(diffDigits.begin());
+        }
+    } else {
         int borrow = 0;
     
         for (int i = digits2.size() - 1; i >= 0; --i) {
@@ -415,12 +428,27 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
             diffDigitsFraction.insert(diffDigitsFraction.begin(), diff);
         }
 
-        if (borrow > 0){
-            std::vector OnesVector(1, 1);
-            alignVectors_1(diffDigits, OnesVector);
-            diffDigits = subtractDigits(diffDigits, OnesVector, 1);
+        digits1 = convertToDigitsVector(num1Parts.first);
+        digits2 = convertToDigitsVector(num2Parts.first);
+
+        // Добавляем нули в конец векторов, чтобы выровнять их по длине
+        alignVectors_1(digits1, digits2);
+    
+        for (int i = digits2.size() - 1; i >= 0; --i) {
+            int diff = digits2[i] - digits1[i] - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else {
+                borrow = 0;
+            }
+            diffDigits.insert(diffDigits.begin(), diff);
         }
 
+        // Удаляем ведущие нули
+        while (!diffDigits.empty() && diffDigits.front() == 0) {
+            diffDigits.erase(diffDigits.begin());
+        }
     }
 
     // Преобразуем результаты в строки
